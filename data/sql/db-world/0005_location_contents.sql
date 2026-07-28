27953a47ed70516aca940574d41e46a85c4465c9
Conan1996-2 <71103778+Conan1996-2@users.noreply.github.com>
2026-07-28 15:48:43 -0400

    Fix coordinates for Talrendis Outpost entry

--- data/sql/db-world/0005_location_contents.sql
+++ data/sql/db-world/0005_location_contents.sql
@@ -1,4 +1,4 @@
 INSERT INTO `guildhouse_locations` (`name`,`mapId`,`zoneId`,`areaId`,`positionX`,`positionY`,`positionZ`,`orientation`,`minX`,`maxX`,`minY`,`maxY`,`price`,`enabled`) VALUES
 ('GM Island', 1, 876, 876, 16222.57, 16265.91, 13.21, 0, 16144, 16393, 16139, 16538, 1000000, 1),
 ('Valormok', 37, 268, 268, 88.38, 915.82, 338.12, 0, -200, 460, 460, 1250, 10000000, 1),
-('Talrendis Outpost', 37, 268, 268, 1144.43, 240, 353, 0, 660, 1347, -165, 534, 10000000, 1);
+('Talrendis Outpost', 37, 268, 268, 1144.43, 240, 353, 0, 665, 1347, -165, 534, 10000000, 1);
