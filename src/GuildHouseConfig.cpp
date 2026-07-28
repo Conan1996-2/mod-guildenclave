0b18dd1dfede571ea5d8bfea129ad384a8a776b4
Conan1996-2 <71103778+Conan1996-2@users.noreply.github.com>
2026-07-27 23:12:47 -0400

    Refactor GuildHouseConfig by removing cost options
    Removed unused cost variables and updated related methods.

--- src/GuildHouseConfig.cpp
+++ src/GuildHouseConfig.cpp
@@ -13,11 +13,6 @@ void GuildHouseConfig::Load()
 {
     _enabled = sConfigMgr->GetOption<bool>("GuildHouse.Enable", false);
     _free = sConfigMgr->GetOption<bool>("GuildHouse.Free", false);
-    _loadAll = sConfigMgr->GetOption<bool>("GuildHouse.LoadAll", false);
-
-    _npcCost = sConfigMgr->GetOption<uint64_t>("GuildHouse.Cost.NPC", 1000000ULL);
-    _portalCost = sConfigMgr->GetOption<uint64_t>("GuildHouse.Cost.Portal", 10000000ULL);
-    _objectCost = sConfigMgr->GetOption<uint64_t>("GuildHouse.Cost.Object", 500000ULL);
 
     _refundPercent = sConfigMgr->GetOption<double>("GuildHouse.Refund", .8);
 
@@ -34,27 +29,12 @@ bool GuildHouseConfig::IsEnabled() const
 
 bool GuildHouseConfig::IsFree() const
 {
-    return _free;
+    return _free > 0;
 }
 
 bool GuildHouseConfig::LoadAll() const
 {
-    return _loadAll;
-}
-
-uint64_t GuildHouseConfig::GetNpcCost() const
-{
-    return _npcCost;
-}
-
-uint64_t GuildHouseConfig::GetPortalCost() const
-{
-    return _portalCost;
-}
-
-uint64_t GuildHouseConfig::GetObjectCost() const
-{
-    return _objectCost;
+    return _free == 2;
 }
 
 double GuildHouseConfig::GetRefundPercent() const
