52a83071cb86bdc8057b4bdafeaa30e06ab93c7b
Conan1996-2 <71103778+Conan1996-2@users.noreply.github.com>
2026-08-07 00:31:24 -0400

    Implement NPC movement control in guild house

---
+++ src/GuildHouseNPCMovement.cpp
@@ -0,0 +1,54 @@
+#include "ScriptMgr.h"
+#include "Player.h"
+#include "Creature.h"
+#include "MotionMaster.h"
+#include "GuildHouseMgr.h"
+
+class GuildHouseNPCMovement : public PlayerScript
+{
+public:
+    GuildHouseNPCMovement() : PlayerScript("GuildHouseNPCMovement") {}
+
+    void OnCreatureInteraction(Player* player, Creature* creature) override
+    {
+        if (!creature)
+            return;
+
+        uint32 spawnId = creature->GetSpawnId();
+
+        // Check if this is one of your guild house NPCs
+        if (!sGuildHouseMgr.IsGuildHouseCreature(spawnId))
+            return;
+
+        // Stop wandering
+        creature->StopMoving();
+        creature->GetMotionMaster()->MoveIdle();
+
+        // Resume after 30 seconds
+        creature->m_Events.AddEvent(new ResumeCreatureMovementEvent(creature), creature->m_Events.CalculateTime(30000));
+    }
+};
+
+class ResumeCreatureMovementEvent : public BasicEvent
+{
+public:
+    explicit ResumeCreatureMovementEvent(Creature* creature) : _creature(creature) {}
+
+    bool Execute(uint64 /*time*/, uint32 /*diff*/) override
+    {
+        if (_creature && _creature->IsInWorld())
+        {
+            _creature->GetMotionMaster()->Initialize();
+        }
+
+        return true;
+    }
+
+private:
+    Creature* _creature;
+};
+
+void AddSC_GuildHouseNPCMovementScripts()
+{
+   new GuildHouseNPCMovement();
+}
