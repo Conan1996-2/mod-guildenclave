e70e61daddbd9886756ff165dca32a4b3b9b6b6c
Conan1996-2 <71103778+Conan1996-2@users.noreply.github.com>
2026-08-07 00:33:54 -0400

    Refactor ResumeCreatureMovementEvent and add GuildHouseNPCMovement

--- src/GuildHouseNPCMovement.h
+++ src/GuildHouseNPCMovement.h
@@ -1,23 +1,33 @@
+#ifndef GUILD_HOUSE_NPC_MOVEMENT_H
+#define GUILD_HOUSE_NPC_MOVEMENT_H
+
+#include "ScriptMgr.h"
+#include "BasicEvent.h"
+
+class Player;
+class Creature;
+
 class ResumeCreatureMovementEvent : public BasicEvent
 {
 public:
-    explicit ResumeCreatureMovementEvent(Creature* creature) : _creature(creature) {}
+    explicit ResumeCreatureMovementEvent(Creature* creature);
 
-    bool Execute(uint64 /*time*/, uint32 /*diff*/) override
-    {
-        if (_creature && _creature->IsInWorld())
-        {
-            _creature->GetMotionMaster()->Initialize();
-        }
-
-        return true;
-    }
+    bool Execute(uint64 /*time*/, uint32 /*diff*/) override;
 
 private:
     Creature* _creature;
 };
 
-void AddSC_GuildHouseNPCMovementScripts()
+
+class GuildHouseNPCMovement : public PlayerScript
 {
-   // new GuildHouseNPCMovement();
-}
+public:
+    GuildHouseNPCMovement();
+
+    void OnCreatureInteraction(Player* player, Creature* creature) override;
+};
+
+
+void AddSC_GuildHouseNPCMovementScripts();
+
+#endif // GUILD_HOUSE_NPC_MOVEMENT_H
