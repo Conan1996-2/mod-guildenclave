1217d94709cc0ebe3eee11bddb6a2f7312de6d22
Conan1996-2 <71103778+Conan1996-2@users.noreply.github.com>
2026-08-06 23:47:40 -0400

    Remove AddSC_GuildHouseMgr call from scripts
    Comment out AddSC_GuildHouseMgr function call in AddGuildHouseScripts.

--- src/loader.cpp
+++ src/loader.cpp
@@ -6,20 +6,18 @@ void AddSC_GuildHouseWorld();
 void AddSC_GuildHouseBroker();
 void AddSC_GuildHouseSalesman();
 void AddSC_GuildHouseCommands();
-void AddSC_GuildHouseMgr();
 void AddSC_GuildHouseScripts();
 
 void AddGuildHouseScripts()
 {
     //
-    // Startup
+    // World
     //
     AddSC_GuildHouseWorld();
 
     //
     // Systems
     //
-//    AddSC_GuildHouseMgr();
     AddSC_GuildHouseScripts();
 
     //
