131b9a1d2e1b333e63906f58b58ec998499f72bd
Conan1996-2 <71103778+Conan1996-2@users.noreply.github.com>
2026-08-07 00:15:28 -0400

    Add ResumeCreatureMovementEvent class and script

---
+++ src/GuildHouseNPCMovement.cpp
@@ -0,0 +1,24 @@
+class ResumeCreatureMovementEvent : public BasicEvent
+{
+public:
+    explicit ResumeCreatureMovementEvent(Creature* creature)
+        : _creature(creature) {}
+
+    bool Execute(uint64 /*time*/, uint32 /*diff*/) override
+    {
+        if (_creature && _creature->IsInWorld())
+        {
+            _creature->GetMotionMaster()->Initialize();
+        }
+
+        return true;
+    }
+
+private:
+    Creature* _creature;
+};
+
+void AddGuildHouseNPCMovementScripts()
+{
+    new GuildHouseNPCMovement();
+}
