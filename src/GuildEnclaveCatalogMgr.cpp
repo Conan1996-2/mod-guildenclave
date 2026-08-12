a4af92f9297b69cf331aa4bc0c87d048dff42eed
Conan1996-2 <71103778+Conan1996-2@users.noreply.github.com>
2026-08-11 21:41:38 -0400

    Fix SQL query for guildenclave_catalog_asset

--- src/GuildEnclaveCatalogMgr.cpp
+++ src/GuildEnclaveCatalogMgr.cpp
@@ -77,7 +77,7 @@ void GuildEnclaveCatalogMgr::Load()
     //
     uint16_t componentCount = 0;
     if (QueryResult result = WorldDatabase.Query("SELECT componentId,catalogId,spawnFlags,behaviorFlags,entryId,displayId,scale,scriptType,scriptData,xOffset,yOffset,zOffset,oOffset,targetMapId,targetX,targetY,targetZ,targetO,childCatalogId,sortOrder "
-        "FROM guildhouse_catalog_asset ORDER BY sortOrder"))
+        "FROM guildenclave_catalog_asset ORDER BY sortOrder"))
     {
         do
         {
