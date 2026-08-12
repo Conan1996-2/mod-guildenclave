1d86425d655882186302e2c298ff841f572b527d
Conan1996-2 <71103778+Conan1996-2@users.noreply.github.com>
2026-08-11 21:43:42 -0400

    Rename Guild House NPCs to Guild Enclave NPCs

--- data/sql/db-world/0002_guildenclave_npc.sql
+++ data/sql/db-world/0002_guildenclave_npc.sql
@@ -1,7 +1,7 @@
 DELETE FROM creature_template WHERE entry=900000;
 
 INSERT INTO creature_template (entry, name, subname, minlevel, maxlevel, faction, npcflag, unit_class, type, speed_walk, speed_run, AIName, ScriptName)
-VALUES (900000, 'Guild House Broker', 'Guild Services', 80, 80, 35, 1, 1, 7, 1, 1.14286, '', 'GuildHouseBroker');
+VALUES (900000, 'Guild Enclave Broker', 'Guild Services', 80, 80, 35, 1, 1, 7, 1, 1.14286, '', 'GuildEnclaveBroker');
 
 DELETE FROM creature_template_model WHERE CreatureID=900000;
 
@@ -11,7 +11,7 @@ VALUES (900000, 0, 25901, 1.0, 1.0);
 DELETE FROM creature_template WHERE entry=900001;
 
 INSERT INTO creature_template (entry, name, subname, minlevel, maxlevel, faction, npcflag, unit_class, type, speed_walk, speed_run, AIName, ScriptName)
-VALUES (900001, 'Guild House Broker', 'Guild Services', 80, 80, 35, 1, 1, 7, 1, 1.14286, '', 'GuildHouseBroker');
+VALUES (900001, 'Guild Enclave Broker', 'Guild Services', 80, 80, 35, 1, 1, 7, 1, 1.14286, '', 'GuildEnclaveBroker');
 
 DELETE FROM creature_template_model WHERE CreatureID=900001;
 
@@ -21,7 +21,7 @@ VALUES (900001, 0, 25901, 1.0, 1.0);
 DELETE FROM creature_template WHERE entry=900002;
 
 INSERT INTO creature_template (entry, name, subname, minlevel, maxlevel, faction, npcflag, unit_class, type, speed_walk, speed_run, AIName, ScriptName)
-VALUES (900002, 'Guild House Salesman', 'Guild Services', 80, 80, 35, 1, 1, 7, 1, 1.14286, '', 'GuildHouseSalesman');
+VALUES (900002, 'Guild Enclave Salesman', 'Guild Services', 80, 80, 35, 1, 1, 7, 1, 1.14286, '', 'GuildEnclaveSalesman');
 
 DELETE FROM creature_template_model WHERE CreatureID=900002;
 
@@ -31,7 +31,7 @@ VALUES (900002, 0, 25901, 1.0, 1.0);
 DELETE FROM creature_template WHERE entry=900003;
 
 INSERT INTO creature_template (entry, name, subname, minlevel, maxlevel, faction, npcflag, unit_class, type, speed_walk, speed_run, AIName, ScriptName)
-VALUES (900003, 'Guild House Salesman', 'Guild Services', 80, 80, 35, 1, 1, 7, 1, 1.14286, '', 'GuildHouseSalesman');
+VALUES (900003, 'Guild Enclave Salesman', 'Guild Services', 80, 80, 35, 1, 1, 7, 1, 1.14286, '', 'GuildEnclaveSalesman');
 
 DELETE FROM creature_template_model WHERE CreatureID=900003;
 
