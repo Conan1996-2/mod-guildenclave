a9ac865f081c25ffbdc501f973d4ce81c0177985
Conan1996-2 <71103778+Conan1996-2@users.noreply.github.com>
2026-08-16 18:16:28 -0400

    Remove ScriptType and ScriptData from struct
    Commented out ScriptType and ScriptData members.

--- src/GuildEnclaveTypes.h
+++ src/GuildEnclaveTypes.h
@@ -24,9 +24,6 @@ struct GHCatalogAsset
 
     GHSpawnFlags SpawnFlags = GH_SPAWN_NONE;
     GHBehaviorFlags BehaviorFlags = GH_BEHAVIOR_NONE;
-    GHScriptType ScriptType = GH_SCRIPT_NONE;
-
-    std::string ScriptData;
 
     float XOffset = 0.0f;
     float YOffset = 0.0f;
@@ -34,13 +31,15 @@ struct GHCatalogAsset
     float OOffset = 0.0f;
 
     uint16_t w = 0;
-
+/*
+    GHScriptType ScriptType = GH_SCRIPT_NONE;
+    std::string ScriptData;
     uint32_t TargetMap = 0;
     float TargetX = 0.0f;
     float TargetY = 0.0f;
     float TargetZ = 0.0f;
     float TargetO = 0.0f;
-
+*/
     uint32_t ChildCatalogId = 0;
     uint16_t SortOrder = 0;
 };
