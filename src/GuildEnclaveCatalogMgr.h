c981e2dbfee196380cebe74d88000f24c7d61460
Conan1996-2 <71103778+Conan1996-2@users.noreply.github.com>
2026-08-21 19:31:15 -0400

    Change _categories to store vector of GHCategory

--- src/GuildEnclaveCatalogMgr.h
+++ src/GuildEnclaveCatalogMgr.h
@@ -36,7 +36,8 @@ private:
     // =====================================================
     // Catalog data
     // =====================================================
-    std::unordered_map<uint32_t, GHCategory> _categories;
+    //std::unordered_map<uint32_t, GHCategory> _categories;
+    std::unordered_map<uint32_t, std::vector<GHCategory>> _categories;
     std::unordered_map<uint32_t, GHCatalog> _catalogs;
 };
 
