9aca496def3e302c95366f63fd38723a85bba359
Conan1996-2 <71103778+Conan1996-2@users.noreply.github.com>
2026-08-21 15:13:07 -0400

    Fix capitalization of AddAsset function call

--- src/GuildEnclaveMgr.cpp
+++ src/GuildEnclaveMgr.cpp
@@ -798,7 +798,7 @@ bool GuildEnclaveMgr::LoadBuild(Player* player)
         do
         {
             Field* fields = result->Fetch();
-            uint32_t assetId = Addasset(player, fields[0].Get<uint32>(), fields[1].Get<float>(), fields[2].Get<float>(), fields[3].Get<float>(), fields[4].Get<float>(), fields[5].Get<float>(), false);
+            uint32_t assetId = AddAsset(player, fields[0].Get<uint32>(), fields[1].Get<float>(), fields[2].Get<float>(), fields[3].Get<float>(), fields[4].Get<float>(), fields[5].Get<float>(), false);
         }while(result->NextRow());
     }
 
