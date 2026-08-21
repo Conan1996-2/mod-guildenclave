c348836551015b357279d3409fc3f030ca579306
Conan1996-2 <71103778+Conan1996-2@users.noreply.github.com>
2026-08-21 13:11:29 -0400

    Modify RemoveAsset function to include refund parameter

--- src/GuildEnclaveMgr.h
+++ src/GuildEnclaveMgr.h
@@ -112,7 +112,7 @@ private:
     std::unordered_map<uint32_t, GHLocation> _locations;
 
     uint32_t AddAsset(Player* player, uint32_t catalogId, bool charge);
-    bool RemoveAsset(Player* player, uint32_t assetId);
+    bool RemoveAsset(Player* player, uint32_t assetId, bool refund);
 };
 
 #define sGuildEnclaveMgr GuildEnclaveMgr::Instance()
