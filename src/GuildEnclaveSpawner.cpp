3516ab7ed441105d2df08d1ea4ee92d5a05fce2f
Conan1996-2 <71103778+Conan1996-2@users.noreply.github.com>
2026-08-25 22:35:38 -0400

    Add logging for component details in GuildEnclaveSpawner
    Added logging for component details during server loading.

--- src/GuildEnclaveSpawner.cpp
+++ src/GuildEnclaveSpawner.cpp
@@ -63,6 +63,8 @@ bool GuildEnclaveSpawner::SpawnAsset(uint32_t guildId, uint32_t assetId, uint32_
 
     for(auto const& component : catalog->Components)
     {
+        LOG_INFO("server.loading", ">>component {} catalog {} entry {} spawn {} behavior {}", component.ComponentId, component.CatalogId, component.Entry, component.SpawnFlags, component.BehanviorFlags, house->Faction);
+        
         if (!GuildEnclaveUtil::HasFlag(component.BehaviorFlags, GH_FACTION_NEUTRAL) || GuildEnclaveUtil::HasFlag(component.BehaviorFlags, static_cast<GHBehaviorFlags>(house->Faction)))
                 continue;
 
